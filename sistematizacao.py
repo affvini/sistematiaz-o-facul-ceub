1aa73f9d84601f1226e7bf21450153863f682d7e
Vinicius Veras <vinicius.f.veras@gmail.com>
2022-03-17 14:54:45 -0300

    Update sistematizacao.py

--- sistematizacao.py
+++ sistematizacao.py
@@ -8,39 +8,51 @@ Milena = float(input('Insira os valores de venda da vendedora Milena: '))
 salario_fixo_vendedor = 1000
 salario_fixo_gerente = 2000
 
-
-if Emily and Larissa and Rafael and Milena < 5000:
+#Vendedora Emily:
+if Emily < 5000:
     total_emily = Emily * 0.01 + salario_fixo_vendedor
-    total_larissa = Larissa * 0.01 + salario_fixo_vendedor
-    total_rafael = Rafael * 0.01 + salario_fixo_vendedor
-    total_milena = Milena * 0.01 + salario_fixo_vendedor
-
     print('Salário da Emily (Vendedora) = R$', total_emily)
-    print('Salário da Larissa (Vendedora) = R$', total_larissa)
-    print('Salário do Rafael (Vendedor) = R$', total_rafael)
-    print('Salário da Milena (Vendedora) = R$', total_milena)
-
-elif Emily and Larissa and Rafael and Milena > 5000 and Emily and Larissa and Rafael and Milena < 10000:
+elif Emily > 5000 < 10000:
     total_emily = Emily * 0.015 + salario_fixo_vendedor
-    total_larissa = Larissa * 0.015 + salario_fixo_vendedor
-    total_rafael = Rafael * 0.015 + salario_fixo_vendedor
-    total_milena = Milena * 0.015 + salario_fixo_vendedor
-
     print('Salário da Emily (Vendedora) = R$', total_emily)
-    print('Salário da Larissa (Vendedora) = R$', total_larissa)
-    print('Salário do Rafael (Vendedor) = R$', total_rafael)
-    print('Salário da Milena (Vendedora) = R$', total_milena)
-
 else:
     total_emily = Emily * 0.2 + salario_fixo_vendedor
-    total_larissa = Larissa * 0.2 + salario_fixo_vendedor
-    total_rafael = Rafael * 0.2 + salario_fixo_vendedor
-    total_milena = Milena * 0.2 + salario_fixo_vendedor
-
     print('Salário da Emily (Vendedora) = R$', total_emily)
+
+#Vendedora Larissa:
+if Larissa < 5000:
+    total_larissa = Larissa * 0.01 + salario_fixo_vendedor
     print('Salário da Larissa (Vendedora) = R$', total_larissa)
+elif Larissa > 5000 < 10000:
+    total_larissa = Larissa * 0.015 + salario_fixo_vendedor
+    print('Salário da Larissa (Vendedora) = R$', total_larissa)
+else:
+    total_larissa = Larissa * 0.2 + salario_fixo_vendedor
+    print('Salário da Larissa (Vendedora) = R$', total_larissa)
+
+#Vendedor Rafael:
+if Rafael < 5000:
+    total_rafael = Rafael * 0.01 + salario_fixo_vendedor
+    print('Salário da Rafael (Vendedor) = R$', total_rafael)
+elif Rafael > 5000 < 10000:
+    total_rafael = Rafael * 0.015 + salario_fixo_vendedor
+    print('Salário da Rafael (Vendedor) = R$', total_rafael)
+else:
+    total_rafael = Rafael * 0.2 + salario_fixo_vendedor
     print('Salário do Rafael (Vendedor) = R$', total_rafael)
+
+
+#Vendedora Milena:
+if Milena < 5000:
+    total_milena = Milena * 0.01 + salario_fixo_vendedor
     print('Salário da Milena (Vendedora) = R$', total_milena)
+elif Milena > 5000 < 10000:
+    total_milena = Milena * 0.015 + salario_fixo_vendedor
+    print('Salário da Milena (Vendedora) = R$', total_milena)
+else:
+    total_milena = Milena * 0.2 + salario_fixo_vendedor
+    print('Salário do Milena (Vendedor) = R$', total_milena)
+
 
 faturamento_total_vendedores = Emily + Larissa + Rafael + Milena
 comissao_gerente = (faturamento_total_vendedores * 0.05) + salario_fixo_gerente
