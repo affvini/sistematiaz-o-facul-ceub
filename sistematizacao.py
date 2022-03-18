cee5a12d885cec51c912181b8f4f60ea2d606767
Vinicius Veras <vinicius.f.veras@gmail.com>
2022-03-18 15:24:26 -0300

    Update sistematizacao.py

--- sistematizacao.py
+++ sistematizacao.py
@@ -4,15 +4,19 @@ Larissa = float(input('Insira os valores de venda da vendedora Larissa: '))
 Rafael = float(input('Insira os valores de venda do vendedor Rafael: '))
 Milena = float(input('Insira os valores de venda da vendedora Milena: '))
 
+vendas = Emily + Larissa + Rafael + Milena
+print('Faturamento total R$ = ', vendas)
+
 #Salário fixo
 salario_fixo_vendedor = 1000
 salario_fixo_gerente = 2000
 
+
 #Vendedora Emily:
-if Emily <= 5000:
+if Emily < 5000:
     total_emily = Emily * 0.01 + salario_fixo_vendedor
     print('Salário da Emily (Vendedora) = R$', total_emily)
-elif Emily < 10000:
+elif Emily >= 5000 and Emily < 10000:
     total_emily = Emily * 0.015 + salario_fixo_vendedor
     print('Salário da Emily (Vendedora) = R$', total_emily)
 else:
@@ -20,10 +24,10 @@ else:
     print('Salário da Emily (Vendedora) = R$', total_emily)
 
 #Vendedora Larissa:
-if Larissa <= 5000:
+if Larissa < 5000:
     total_larissa = Larissa * 0.01 + salario_fixo_vendedor
     print('Salário da Larissa (Vendedora) = R$', total_larissa)
-elif Larissa < 10000:
+elif Larissa >= 5000 and Larissa < 10000:
     total_larissa = Larissa * 0.015 + salario_fixo_vendedor
     print('Salário da Larissa (Vendedora) = R$', total_larissa)
 else:
@@ -31,10 +35,10 @@ else:
     print('Salário da Larissa (Vendedora) = R$', total_larissa)
 
 #Vendedor Rafael:
-if Rafael <= 5000:
+if Rafael < 5000:
     total_rafael = Rafael * 0.01 + salario_fixo_vendedor
     print('Salário da Rafael (Vendedor) = R$', total_rafael)
-elif Rafael < 10000:
+elif Rafael >= 5000 and Rafael < 10000:
     total_rafael = Rafael * 0.015 + salario_fixo_vendedor
     print('Salário da Rafael (Vendedor) = R$', total_rafael)
 else:
@@ -43,10 +47,10 @@ else:
 
 
 #Vendedora Milena:
-if Milena <= 5000:
+if Milena < 5000:
     total_milena = Milena * 0.01 + salario_fixo_vendedor
     print('Salário da Milena (Vendedora) = R$', total_milena)
-elif Milena < 10000:
+elif Milena >= 5000 and Milena < 10000:
     total_milena = Milena * 0.015 + salario_fixo_vendedor
     print('Salário da Milena (Vendedora) = R$', total_milena)
 else:
@@ -54,10 +58,10 @@ else:
     print('Salário do Milena (Vendedor) = R$', total_milena)
 
 
-faturamento_total_vendedores = Emily + Larissa + Rafael + Milena
-comissao_gerente = (faturamento_total_vendedores * 0.05) + salario_fixo_gerente
+
+comissao_gerente = (vendas * 0.05) + salario_fixo_gerente
+total_salarios = float(total_emily + total_larissa + total_milena + total_rafael + comissao_gerente)
 
 
-total_salarios = float(total_emily) + float(total_larissa) + float(total_milena) + float(total_rafael) + float(comissao_gerente)
 print('Salário da Jéssica (gerente) = R$', comissao_gerente)
 print('Total dos salários = ', total_salarios)
